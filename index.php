8d9e6d5493eaf74442112b81f27b07868412b1ae
Jav99 . <januc99@gmail.com>
2025-09-13 13:59:01 +0200

    Cambio nuevo

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-require_once '/includes/functions.php';
+require_once 'includes/functions.php';
 ensure_logged_in();          // Si está logeado, pasa
 header('Location: chat_list.php');   // Si no, lo lleva a login
