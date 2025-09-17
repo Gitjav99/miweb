c854b4d20cd3b5a85c3389e1c5cdd1ce2aa65bda
Jav99 . <januc99@gmail.com>
2025-09-17 12:29:40 +0200

    Cambio nuevo

--- public/dashboard.php
+++ public/dashboard.php
@@ -10,6 +10,7 @@ if (!isset($_SESSION['user_id'])) {
 <head><meta charset="UTF-8"><title>Dashboard</title></head>
 <body>
 <h1>¡Bienvenido, <?= htmlspecialchars($_SESSION['user_usuario']); ?>!</h1>
+<?php print_r(array_keys($_SESSION)); ?>
 <p>Esta es una página protegida.</p>
 <a href="logout.php">Cerrar sesión</a>
 </body>
