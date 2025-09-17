2dfacf42296a3eb5f7e489e46e4978da021e48de
Jav99 . <januc99@gmail.com>
2025-09-17 13:22:05 +0200

    Cambio nuevo

--- public/login.php
+++ public/login.php
@@ -135,6 +135,7 @@ if (!password_verify($password, $user['password_hash'])) {
 
 // 3️⃣ Login correcto – puedes iniciar sesión (ej. $_SESSION)
 session_start();
+$_SESSION['user_id'] = $user['id'];
 $_SESSION['nombre_usuario']= $user["nombre"];
 
 header('Location: dashboard.php'); // Página protegida
